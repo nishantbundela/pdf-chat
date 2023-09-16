f4ef89c6c91cae310e939d53ec927245b536070d
nishantbundela <nishantbundela@gmail.com>
2023-09-16 06:20:43 -0400

    added aws s3 bucket to manage uploaded pdf files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,15 @@ export default async function Home() {
   const isAuth = !!userId
   return (
     <div className="w-screen min-h-screen bg-gradient-to-r from-gray-700 via-gray-900 to-black">
+      <div className="p-4">
+        <UserButton afterSignOutUrl="/" />
+      </div>
+      
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center text-center">
         <div className="flex items-center">
         <h1 className="mr-3 text-5xl font-semibold white-clr">Chat with any PDF</h1>
-        <UserButton afterSignOutUrl="/" />
+        
         </div>
         <div className="flex mt-2">
           {isAuth && <Button variant="green">Go to Chats</Button>}
--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,10 +1,47 @@
 "use client";
 import React from 'react';
+import { Inbox } from "lucide-react";
 import {useDropzone} from 'react-dropzone';
+import { uploadToS3 } from "@/lib/s3";
 
 const FileUpload = () => {
-    return <div className="p-2 bg-white rounded-x1"></div>;
-    };
+    const { getRootProps, getInputProps } = useDropzone({
+        accept: { "application/pdf": [".pdf"] },
+        maxFiles: 1,
+        onDrop: async (acceptedFiles) => {
+            console.log(acceptedFiles);
+            const file = acceptedFiles[0];
+            if (file.size > 10 * 1024 *1024) {
+                alert("File size exceeds limit of 10MB");
+                return;
+            }
+
+            try {
+                const data = await uploadToS3(file);
+                console.log("data", data)
+            } catch (error) {
+                console.log(error);
+            }
+            
+        }
+})
+    return (
+        <div className="p-2 bg-white rounded-xl">
+            <div
+                {...getRootProps({
+                    className:
+                        "border-dashed border-2 rounded-xl cursor-pointer bg-gray-50 py-8 flex justify-center items-center flex-col",
+        })}
+      >
+        <input {...getInputProps()} />
+        
+          <>
+            <Inbox className="w-10 h-10 text-blue-500" />
+            <p className="mt-2 text-sm text-slate-400">Drop PDF Here</p>
+          </>
+      </div>
+    </div>
+        )}
 
 
 export default FileUpload;
